fbb97bc294b85e6b8cce71ed911f32ebbe6428a7
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-05 16:49:24 +0100

    add Navigation to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Navigation from '@/components/ui/Navigation/Navigation'
 import localFont from 'next/font/local'
 import './globals.css'
 
@@ -25,7 +26,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+				<Navigation />
+				{children}
+			</body>
 		</html>
 	)
 }
