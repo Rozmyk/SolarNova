d25c8122b9a8b3932ee3732ee30c29a8aaf260d5
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-09 18:17:48 +0100

    Change size burger btn

--- src/components/ui/Navigation/MobileNav/BurgerButton/BurgerButton.tsx
+++ src/components/ui/Navigation/MobileNav/BurgerButton/BurgerButton.tsx
@@ -8,7 +8,7 @@ const BurgerButton = () => {
 			className='flex flex-col h-10 w-10   rounded-lg bg-[#ccf2de] justify-center items-center group'
 			onClick={() => setIsOpen(!isOpen)}>
 			<div className='h-10 w-5 flex flex-col items-start justify-center'>
-				<div className={`${genericHamburgerLine} ${isOpen ? 'rotate-45 translate-y-1.5 w-5 ' : 'opacity-100 w-2.5'}`} />
+				<div className={`${genericHamburgerLine} ${isOpen ? 'rotate-45 translate-y-1.5 w-5 ' : 'opacity-100 w-3'}`} />
 				<div className={`${genericHamburgerLine} ${isOpen ? 'opacity-0' : 'opacity-100'}`} />
 				<div
 					className={`${genericHamburgerLine} ${isOpen ? '-rotate-45 -translate-y-1.5 opacity-100' : 'opacity-100'}`}
