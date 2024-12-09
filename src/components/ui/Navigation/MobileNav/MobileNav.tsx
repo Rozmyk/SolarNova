d21bcec4852dd9ed227e6139351a6a21b7369faa
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-09 18:16:37 +0100

    New color DarkText and change foregroundColor

--- src/components/ui/Navigation/MobileNav/MobileNav.tsx
+++ src/components/ui/Navigation/MobileNav/MobileNav.tsx
@@ -2,7 +2,7 @@ import HomeButton from '../HomeButton/HomeButton'
 import BurgerButton from './BurgerButton/BurgerButton'
 const MobileNav = () => {
 	return (
-		<div className=' w-full h-20 flex justify-between items-center p-2 bg-[#f0fbf5]'>
+		<div className=' w-full h-20 flex justify-between items-center p-2 bg-foregroundh'>
 			<HomeButton />
 			<BurgerButton />
 		</div>
