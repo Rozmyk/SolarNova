83f30c2afbfab4e6913e2b3b191544d61acdbb24
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-10 11:50:12 +0100

    Create Solutions.tsx

---
+++ src/components/Solutions/Solutions.tsx
@@ -0,0 +1,13 @@
+import Wrapper from '../Wrapper/Wrapper'
+const Solutions = () => {
+	return (
+		<Wrapper>
+			<div className='flex justify-between items-center bg-red-400'>
+				<div className='w-full md:w-1/2'>text</div>
+				<div className='w-full md:w-1/2'>button</div>
+			</div>
+		</Wrapper>
+	)
+}
+
+export default Solutions
