f14dc257bc1f348c0f06c75721a0141c9b8d082b
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-10 17:53:03 +0100

    Add title with button and cards

--- src/components/Solutions/Solutions.tsx
+++ src/components/Solutions/Solutions.tsx
@@ -1,10 +1,20 @@
 import Wrapper from '../Wrapper/Wrapper'
+import TitleText from '../ui/TitleText/TitleText'
+import Button from '../ui/Button/Button'
+import Cards from './Cards/Cards'
 const Solutions = () => {
 	return (
 		<Wrapper>
-			<div className='flex justify-between items-center bg-red-400'>
-				<div className='w-full md:w-1/2'>text</div>
-				<div className='w-full md:w-1/2'>button</div>
+			<div className='flex md:flex-row flex-col justify-between items-center gap-4 mb-8 p-4'>
+				<div className='w-full md:w-1/2'>
+					<TitleText>We Provide Comprehensive Solar Solutions</TitleText>
+				</div>
+				<div className='w-full md:w-1/2 flex justify-end'>
+					<Button>View all services</Button>
+				</div>
+			</div>
+			<div className='w-full  p-8'>
+				<Cards />
 			</div>
 		</Wrapper>
 	)
