a2bf5b18d716ea709855db27b756295642b0535a
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-10 11:58:56 +0100

    Update Button, add capitalize

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,11 +1,12 @@
+'use client'
 import { TbCircleArrowUpRightFilled } from 'react-icons/tb'
 
-const Button = ({ children, reverse, onClick }: { children: string; reverse?: boolean; onClick: () => void }) => {
+const Button = ({ children, reverse, onClick }: { children: string; reverse?: boolean; onClick?: () => void }) => {
 	return reverse ? (
 		<button
 			onClick={onClick}
 			type='button'
-			className='py-2.5 px-6 text-sm bg-[#f7f7f7] text-[#323332] rounded-full cursor-pointer font-medium text-center shadow-xs transition-all duration-500 hover:bg-stone-200 inline-flex items-center gap-1'>
+			className='py-2.5 px-6 text-sm bg-[#f7f7f7] text-[#323332] rounded-full cursor-pointer font-regular text-center shadow-xs transition-all duration-500 hover:bg-stone-200 inline-flex items-center gap-1 capitalize'>
 			{children}
 			<TbCircleArrowUpRightFilled size={25} color='#40cb7f' />
 		</button>
@@ -13,7 +14,7 @@ const Button = ({ children, reverse, onClick }: { children: string; reverse?: bo
 		<button
 			onClick={onClick}
 			type='button'
-			className='py-2.5 px-6 text-sm bg-primary text-white rounded-full cursor-pointer font-medium text-center shadow-xs transition-all duration-500 hover:bg-accent inline-flex items-center gap-1'>
+			className='py-2.5 px-6 text-sm bg-primary text-white rounded-full cursor-pointer font-regular text-center shadow-xs transition-all duration-500 hover:bg-accent inline-flex items-center gap-1 capitalize '>
 			{children}
 			<TbCircleArrowUpRightFilled size={25} color='white' />
 		</button>
