ab028860e0b275db9bf700de402aceff1e9267ae
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-11 17:17:18 +0100

    Add Data

---
+++ data/projectsData.tsx
@@ -0,0 +1,64 @@
+const projectData = [
+	{
+		src: '/projectImage1.jpg',
+		title: 'Solar Firm & Wind Turbine',
+		date: 'December 15, 2024',
+		author: 'Christopher Jack',
+	},
+	{
+		src: '/projectImage2.jpg',
+		title: 'Green Energy Innovations',
+		date: 'November 10, 2024',
+		author: 'Sophia Green',
+	},
+	{
+		src: '/projectImage3.jpg',
+		title: 'Urban Smart Home System',
+		date: 'October 20, 2024',
+		author: 'Michael Brown',
+	},
+	{
+		src: '/projectImage4.jpg',
+		title: 'AI-Powered Healthcare',
+		date: 'September 5, 2024',
+		author: 'Emily White',
+	},
+	{
+		src: '/projectImage5.jpg',
+		title: 'Autonomous Delivery Drone',
+		date: 'August 12, 2024',
+		author: 'Liam Carter',
+	},
+	{
+		src: '/projectImage6.jpg',
+		title: 'Blockchain for Sustainability',
+		date: 'July 22, 2024',
+		author: 'Emma Wilson',
+	},
+	{
+		src: '/projectImage7.jpg',
+		title: 'Virtual Reality Education',
+		date: 'June 15, 2024',
+		author: 'Olivia Martinez',
+	},
+	{
+		src: '/projectImage8.jpg',
+		title: 'Next-Gen Electric Vehicles',
+		date: 'May 30, 2024',
+		author: 'James Lopez',
+	},
+	{
+		src: '/projectImage9.jpg',
+		title: 'Renewable Ocean Energy',
+		date: 'April 18, 2024',
+		author: 'Isabella Thompson',
+	},
+	{
+		src: '/projectImage10.jpg',
+		title: 'Advanced Robotics Solutions',
+		date: 'March 10, 2024',
+		author: 'Noah Johnson',
+	},
+]
+
+export default projectData
---
+++ data/testimonialsData.tsx
@@ -0,0 +1,84 @@
+const testimonialsData = [
+	{
+		src: '/customerAvatar1.jpg',
+		reversed: false,
+		opinion:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt aliquam architecto itaque expedita fugit. Consectetur atque culpa architecto.',
+		author: 'Jennifer Kaya',
+		job: 'Brand Promoter',
+	},
+	{
+		src: '/customerAvatar2.jpg',
+		reversed: true,
+		opinion:
+			'Doloribus illum atque distinctio, ratione ullam tenetur officiis quibusdam commodi numquam? Sed repellat similique adipisci accusantium laborum maiores.',
+		author: 'Michael Roberts',
+		job: 'Software Engineer',
+	},
+	{
+		src: '/customerAvatar3.jpg',
+		reversed: false,
+		opinion:
+			'Reiciendis delectus saepe animi corporis rerum! Vel reprehenderit libero alias, magnam corrupti iusto adipisci voluptatem!',
+		author: 'Sophia Martin',
+		job: 'Marketing Specialist',
+	},
+	{
+		src: '/customerAvatar4.jpg',
+		reversed: true,
+		opinion:
+			'Aliquid atque, impedit tempore temporibus ad quod! Porro laborum voluptate non eveniet, nostrum molestias cupiditate facilis ipsa!',
+		author: 'Liam Brown',
+		job: 'UI/UX Designer',
+	},
+	{
+		src: '/customerAvatar5.jpg',
+		reversed: false,
+		opinion:
+			'Voluptatem, vero, quibusdam atque pariatur molestias repudiandae veniam asperiores eveniet, nesciunt tempora minus molestiae.',
+		author: 'Emma Wilson',
+		job: 'Project Manager',
+	},
+	{
+		src: '/customerAvatar6.jpg',
+		reversed: true,
+		opinion:
+			'Eum, officia ipsa repudiandae exercitationem. Sint amet fuga nulla quam debitis repellendus adipisci sapiente voluptatem!',
+		author: 'Oliver Davis',
+		job: 'Graphic Designer',
+	},
+	{
+		src: '/customerAvatar7.jpg',
+		reversed: false,
+		opinion:
+			'Ipsa labore, dicta nisi recusandae dolor quisquam voluptates possimus. Omnis necessitatibus eaque ducimus deserunt!',
+		author: 'Mia Johnson',
+		job: 'Content Creator',
+	},
+	{
+		src: '/customerAvatar8.jpg',
+		reversed: true,
+		opinion:
+			'Deleniti consectetur dolorum architecto, eveniet similique laborum pariatur neque repellendus, dolore, maxime quod reprehenderit!',
+		author: 'Noah Martinez',
+		job: 'SEO Specialist',
+	},
+	{
+		src: '/customerAvatar9.jpg',
+		reversed: false,
+		opinion:
+			'Magnam nobis velit quidem omnis molestiae repellat ullam voluptatibus quisquam architecto. Eligendi, incidunt deserunt?',
+		author: 'Isabella Clark',
+		job: 'Digital Strategist',
+	},
+	{
+		src: '/customerAvatar10.jpg',
+		reversed: true,
+		opinion:
+			'Voluptates, maiores facilis! Recusandae, iure id! Deserunt alias veritatis atque quis officia. Illum vitae maiores.',
+		author: 'James Lopez',
+		job: 'Photographer',
+	},
+]
+
+export default testimonialsData
