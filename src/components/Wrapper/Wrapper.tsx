4b947323aa43e97ade02302b85dc5043e28bfdbe
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-11 14:12:31 +0100

    Update wrapper

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -1,11 +1,9 @@
 import { ReactNode } from 'react'
 
-const Wrapper = ({ children, withoutBorder }: { children: ReactNode; withoutBorder: boolean }) => {
+const Wrapper = ({ children }: { children: ReactNode }) => {
 	return (
-		<div className='bg-white min-w-[100vw] min-h-[100vh] max-w-[100vh] flex flex-col justify-center items-center'>
-			<div className={`max-w-6xl w-6xl mt-24 mb-24 w-full ${withoutBorder ? '' : 'border border-solid border-border'}`}>
-				{children}
-			</div>
+		<div className='bg-transparent max-w-screen  overflow-hidden  flex flex-col justify-center items-center '>
+			<div className={`max-w-6xl w-6xl mt-20 mb-20 w-full `}>{children}</div>
 		</div>
 	)
 }
