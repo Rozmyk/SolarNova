9fe527903e894d400929ae64cd8fac330bf6e8f3
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-13 17:49:00 +0100

    Add button redirect

--- src/components/Solutions/Solutions.tsx
+++ src/components/Solutions/Solutions.tsx
@@ -1,16 +1,24 @@
+'use client'
 import Wrapper from '../Wrapper/Wrapper'
 import TitleText from '../ui/TitleText/TitleText'
 import Button from '../ui/Button/Button'
 import Cards from './Cards/Cards'
+import { useRouter } from 'next/navigation'
 const Solutions = () => {
+	const router = useRouter()
 	return (
 		<Wrapper>
-			<div className='flex md:flex-row flex-col justify-between items-center gap-4 mb-8 p-4'>
+			<div className='flex md:flex-row flex-col justify-between items-center gap-4 mb-2 p-4'>
 				<div className='w-full md:w-1/2'>
 					<TitleText>We Provide Comprehensive Solar Solutions</TitleText>
 				</div>
 				<div className='w-full md:w-1/2 flex justify-end'>
-					<Button>View all services</Button>
+					<Button
+						onClick={() => {
+							router.push('/services')
+						}}>
+						View all services
+					</Button>
 				</div>
 			</div>
 			<div className='w-full  p-8'>
