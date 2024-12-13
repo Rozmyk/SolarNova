3f566add886f37a0c6f4af83eb0594f92699d841
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-13 17:48:25 +0100

    Update SingleNumbered content and change decorative item

--- src/components/PerformanceStats/PerformanceStats.tsx
+++ src/components/PerformanceStats/PerformanceStats.tsx
@@ -4,7 +4,7 @@ import { GoNorthStar } from 'react-icons/go'
 import Clients from './Clients/Clients'
 const SingleNumberedTile = ({ number, title }: { number: string; title: string }) => {
 	return (
-		<div className='w-full h-44 flex flex-col justify-center items-center bg-[#f7f9f6] rounded-lg drop-shadow-md m-2'>
+		<div className='w-full h-44 flex flex-col justify-center items-center bg-[#f7f9f6] rounded-lg drop-shadow-md m-2 '>
 			<div>
 				<p className='text-center text-black font-medium text-4xl mb-4'>{number}</p>
 				<p className='text-darkText text-center'>{title}</p>
@@ -23,17 +23,22 @@ const PerformanceStats = () => {
 					<div className='w-full md:w-1/2  flex flex-col '>
 						<div className='flex md:flex-row flex-col justify-between items-center'>
 							<SingleNumberedTile number='1.5K' title='Instalation' />
-							<SingleNumberedTile number='1.5k' title='Instalation' />
+							<SingleNumberedTile number='20' title='Country served' />
 						</div>
 						<div className='flex md:flex-row flex-col justify-between items-center'>
-							<SingleNumberedTile number='1.5k' title='Instalation' />
-							<SingleNumberedTile number='1.5k' title='Instalation' />
+							<SingleNumberedTile number='60%' title='Energy saving' />
+							<SingleNumberedTile number='86' title='Our partner' />
 						</div>
 					</div>
 				</div>
 			</Wrapper>
 
-			<GoNorthStar className='absolute left-0 bottom-[-100px] rotate-45 opacity-5 z-0  ' size={500} color='black' />
+			<GoNorthStar
+				style={{ opacity: 0.02 }}
+				className='absolute left-0 bottom-96 rotate-45  z-0  '
+				size={500}
+				color='black'
+			/>
 			<Clients />
 		</div>
 	)
