512e3defa4de289cddf9eeb942047f63bbec07a2
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-13 13:46:52 +0100

    Add buttons logic

--- src/components/Projects/CarouselButtons/CarouselButtonts.tsx
+++ src/components/Projects/CarouselButtons/CarouselButtonts.tsx
@@ -1,12 +1,15 @@
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md'
+import 'swiper/css'
 
-const CarouselButtons = () => {
+const CarouselButtons = ({ onPrev, onNext }: { onPrev: () => void; onNext: () => void }) => {
 	return (
 		<div className='flex justify-center items-center gap-4'>
-			<button className='w-10 h-10 rounded-full bg-transparent  border-primary border border-solid flex justify-center items-center'>
+			<button
+				onClick={onPrev}
+				className=' w-10 h-10 rounded-full bg-transparent  border-primary border border-solid flex justify-center items-center'>
 				<MdKeyboardArrowLeft size={20} color='#40cb7f' />
 			</button>
-			<button className='w-10 h-10 rounded-full bg-primary flex justify-center items-center'>
+			<button onClick={onNext} className='w-10 h-10 rounded-full bg-primary flex justify-center items-center'>
 				<MdKeyboardArrowRight size={20} />
 			</button>
 		</div>
