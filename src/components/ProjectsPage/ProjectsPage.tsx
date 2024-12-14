b6bdd1d0a793afd6003b9ec4a462ef873e37e54c
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-14 18:28:44 +0100

    Add project page

--- src/components/ProjectsPage/ProjectsPage.tsx
+++ src/components/ProjectsPage/ProjectsPage.tsx
@@ -1,8 +1,19 @@
 import Wrapper from '../Wrapper/Wrapper'
+import projectData from '../../../data/projectsData'
+import ProjectCard from '../Projects/ProjectCard/ProjectCard'
+
 const ProjectsPage = () => {
 	return (
 		<Wrapper>
-			<div className='bg-red-500'>projects</div>
+			<div className='w-full flex justify-center items-center'>
+				<div
+					className='grid w-full gap-8 max-w-screen-lg 
+                  grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
+					{projectData.map(project => (
+						<ProjectCard key={project.id} {...project} />
+					))}
+				</div>
+			</div>
 		</Wrapper>
 	)
 }
