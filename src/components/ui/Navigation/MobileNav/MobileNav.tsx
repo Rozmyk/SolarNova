dc06066d83616aaf1aafef8b6f57ae3add69c62a
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-15 17:51:46 +0100

    Add mobile menu logic

--- src/components/ui/Navigation/MobileNav/MobileNav.tsx
+++ src/components/ui/Navigation/MobileNav/MobileNav.tsx
@@ -1,9 +1,23 @@
 import HomeButton from '../HomeButton/HomeButton'
 import BurgerButton from './BurgerButton/BurgerButton'
 import Content from './Content/Content'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 const MobileNav = () => {
 	const [isOpen, setIsOpen] = useState(false)
+	useEffect(() => {
+		if (isOpen) {
+			document.body.style.position = 'fixed'
+			document.body.style.overflow = 'hidden'
+		} else {
+			document.body.style.position = ''
+			document.body.style.overflow = ''
+		}
+
+		return () => {
+			document.body.style.position = ''
+			document.body.style.overflow = ''
+		}
+	}, [isOpen])
 	return (
 		<div className=' w-full  h-20 flex justify-between items-center p-2 bg-foreground'>
 			<HomeButton />
