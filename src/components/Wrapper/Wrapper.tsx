10cdaaaf8c750b3d0a034159388eb30bdfd31510
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-15 17:59:26 +0100

    Add additional classname props

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from 'react'
 
-const Wrapper = ({ children }: { children: ReactNode }) => {
+const Wrapper = ({ children, className }: { children: ReactNode; className?: string }) => {
 	return (
 		<div className='bg-transparent max-w-screen  overflow-hidden  flex flex-col justify-center items-center '>
-			<div className={`max-w-6xl w-6xl mt-20 mb-20 w-full `}>{children}</div>
+			<div className={`max-w-6xl w-6xl mt-20 mb-20 w-full ${className} `}>{children}</div>
 		</div>
 	)
 }
