a0d429d2e60c740c489b52284f835b4bf32ea048
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-16 16:48:13 +0100

    Add background gradient

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -5,40 +5,43 @@ import { RiArrowDownSLine } from 'react-icons/ri'
 import Button from '../ui/Button/Button'
 import TitleText from '../ui/TitleText/TitleText'
 import { useRouter } from 'next/navigation'
+
 const SingleRatingComponent = ({ number, text }: { number: string; text: string }) => {
 	return (
 		<div className='flex flex-col justify-center items-start'>
 			<p className='text-secondary text-4xl font-semibold'>{number}</p>
-			<p className='text-darkText '>{text}</p>
+			<p className='text-secondary font-semibold'>{text}</p>
 		</div>
 	)
 }
+
 const AboutUs = () => {
 	const router = useRouter()
+
 	return (
-		<div className='bg-foreground w-full relative p-4 overflow-hidden'>
-			<div className='absolute opacity-10 top-5 md:left-10 right-0 '>
+		<div className='relative w-full overflow-hidden p-4 bg-[radial-gradient(circle,_var(--tw-gradient-stops))] from-[#f2fff4] to-white'>
+			<div className='absolute opacity-10 top-5 md:left-10 right-0'>
 				<PiCornersInFill size={150} color='#40cb7f' />
 			</div>
 			<Wrapper>
 				<TitleText>About Us</TitleText>
-				<div className='flex flex-col w-full md:flex-row justify-center md:justify-between items-center md:items-start gap-10 mb-4 '>
+				<div className='flex flex-col w-full md:flex-row justify-center md:justify-between items-center md:items-start gap-10 mb-4'>
 					<div className='w-full md:w-1/2'>
-						<p className='text-darkText font-regular'>
-							We strive to be your trusted partner in the tranisition to a greener tomorrow.
+						<p className='text-secondary font-semibold'>
+							We strive to be your trusted partner in the transition to a greener tomorrow.
 						</p>
 						<div className='flex justify-start items-center gap-8 mb-12 mt-4'>
 							<SingleRatingComponent number='4.9/5' text='Client rating' />
 							<SingleRatingComponent number='06' text='Experience Years' />
 						</div>
 					</div>
-					<div className='w-full md:w-1/2 mt-4 md:mt-0'>
-						<p className='text-darkText font-regular mb-4'>
+					<div className='w-full md:w-1/2 mt-4 md:mt-0 text-secondary font-semibold'>
+						<p className='font-regular mb-4'>
 							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quisquam sed cumque cupiditate nesciunt quis
 							doloribus praesentium quae possimus dolor iure adipisci, repellendus vero laudantium at sunt temporibus
 							eveniet saepe?
 						</p>
-						<p className='text-darkText font-regular'>
+						<p className='font-regular'>
 							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quisquam sed cumque cupiditate nesciunt quis
 							doloribus praesentium quae possimus dolor iure adipisci, repellendus vero laudantium at sunt temporibus
 							eveniet saepe?
@@ -52,7 +55,8 @@ const AboutUs = () => {
 					Learn More
 				</Button>
 			</Wrapper>
-			<div className='absolute opacity-10 bottom-0 right-5 md:right-36  ' style={{ transform: 'rotate(135deg)' }}>
+
+			<div className='absolute opacity-10 bottom-0 right-5 md:right-36' style={{ transform: 'rotate(135deg)' }}>
 				<RiArrowDownSLine size={200} color='#40cb7f' />
 			</div>
 		</div>
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -17,63 +17,65 @@ const Projects = () => {
 	const router = useRouter()
 
 	return (
-		<Wrapper>
-			<div className='flex md:flex-row flex-col justify-between items-center gap-4 mb-2 p-4'>
-				<div className='w-full md:w-1/2'>
-					<TitleText>Successful Renewable energy projects</TitleText>
+		<div className='relative w-full overflow-hidden p-4 bg-[radial-gradient(circle,_var(--tw-gradient-stops))] from-[#f2fff4] to-white'>
+			<Wrapper>
+				<div className='flex md:flex-row flex-col justify-between items-center gap-4 mb-2 p-4'>
+					<div className='w-full md:w-1/2'>
+						<TitleText>Successful Renewable energy projects</TitleText>
+					</div>
+					<div className='w-full md:w-1/2 flex justify-end'>
+						<CarouselButtons
+							onPrev={() => swiperRef.current?.slidePrev()}
+							onNext={() => swiperRef.current?.slideNext()}
+						/>
+					</div>
 				</div>
-				<div className='w-full md:w-1/2 flex justify-end'>
-					<CarouselButtons
-						onPrev={() => swiperRef.current?.slidePrev()}
-						onNext={() => swiperRef.current?.slideNext()}
-					/>
+
+				<Swiper
+					breakpoints={{
+						0: {
+							slidesPerView: 1,
+							centeredSlides: true,
+							spaceBetween: 0,
+						},
+						750: {
+							slidesPerView: 2,
+							centeredSlides: false,
+							spaceBetween: 25,
+						},
+						1100: {
+							slidesPerView: 3,
+							centeredSlides: false,
+							spaceBetween: 50,
+						},
+					}}
+					loop
+					onSwiper={swiper => (swiperRef.current = swiper)}>
+					{projectData.map(project => (
+						<SwiperSlide key={project.title}>
+							<div className='w-full h-full flex justify-center items-center '>
+								<ProjectCard
+									id={project.id}
+									src={project.src}
+									title={project.title}
+									date={project.date}
+									author={project.author}
+								/>
+							</div>
+						</SwiperSlide>
+					))}
+				</Swiper>
+
+				<div className='flex justify-center items-center w-full p-4 mt-8'>
+					<Button
+						onClick={() => {
+							router.push('/projects')
+						}}>
+						View all projects
+					</Button>
 				</div>
-			</div>
-
-			<Swiper
-				breakpoints={{
-					0: {
-						slidesPerView: 1,
-						centeredSlides: true,
-						spaceBetween: 0,
-					},
-					750: {
-						slidesPerView: 2,
-						centeredSlides: false,
-						spaceBetween: 25,
-					},
-					1100: {
-						slidesPerView: 3,
-						centeredSlides: false,
-						spaceBetween: 50,
-					},
-				}}
-				loop
-				onSwiper={swiper => (swiperRef.current = swiper)}>
-				{projectData.map(project => (
-					<SwiperSlide key={project.title}>
-						<div className='w-full h-full flex justify-center items-center '>
-							<ProjectCard
-								id={project.id}
-								src={project.src}
-								title={project.title}
-								date={project.date}
-								author={project.author}
-							/>
-						</div>
-					</SwiperSlide>
-				))}
-			</Swiper>
-
-			<div className='flex justify-center items-center w-full p-4 mt-8'>
-				<Button
-					onClick={() => {
-						router.push('/projects')
-					}}>
-					View all projects
-				</Button>
-			</div>
-		</Wrapper>
+			</Wrapper>
+		</div>
 	)
 }
 
