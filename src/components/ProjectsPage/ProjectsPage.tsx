53ba451810bf9d62eb4f0210cee266dd6efb7f3e
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-16 15:18:08 +0100

    Create FilterBar to projectPage

--- src/components/ProjectsPage/ProjectsPage.tsx
+++ src/components/ProjectsPage/ProjectsPage.tsx
@@ -1,14 +1,25 @@
 import Wrapper from '../Wrapper/Wrapper'
 import projectData from '../../../data/projectsData'
 import ProjectCard from '../Projects/ProjectCard/ProjectCard'
+import TitleText from '../ui/TitleText/TitleText'
+import FilterBar from './FilterBar/FilterBar'
 
 const ProjectsPage = () => {
 	return (
 		<Wrapper>
-			<div className='w-full flex justify-center items-center'>
+			<div className='w-full flex flex-col justify-center items-center'>
+				<div className='w-full flex md:flex-row flex-col justify-between items-center gap-4 mb-8 p-4 '>
+					<div className='w-full md:w-1/2'>
+						<TitleText>Our Projects</TitleText>
+					</div>
+					<div className='w-full md:w-1/2 flex justify-end'>
+						<p>buttons</p>
+					</div>
+				</div>
+				<FilterBar />
 				<div
 					className='grid w-full gap-8 max-w-screen-lg 
-                  grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
+                  grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-8'>
 					{projectData.map(project => (
 						<ProjectCard key={project.id} {...project} />
 					))}
