2f06fdbdb4266f43bbd6594b419d5de8263ab120
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-16 17:47:51 +0100

    Update projectbar (add timestamp)

--- data/projectsData.tsx
+++ data/projectsData.tsx
@@ -2,9 +2,8 @@ interface Project {
 	id: string
 	src: string
 	title: string
-	date: string
+	date: number
 	author: string
-	category: string
 }
 
 const projectData: Project[] = [
@@ -12,81 +11,71 @@ const projectData: Project[] = [
 		id: 'project-1',
 		src: '/projectImage1.jpg',
 		title: 'Solar Farm Expansion Initiative',
-		date: 'December 15, 2024',
+		date: new Date('December 15, 2024').getTime(),
 		author: 'Christopher Jack',
-		category: 'Solar Energy',
 	},
 	{
 		id: 'project-2',
 		src: '/projectImage2.jpg',
 		title: 'Offshore Wind Turbine Development',
-		date: 'November 10, 2024',
+		date: new Date('November 10, 2024').getTime(),
 		author: 'Sophia Green',
-		category: 'Wind Energy',
 	},
 	{
 		id: 'project-3',
 		src: '/projectImage3.jpg',
 		title: 'Geothermal Energy Plant in Iceland',
-		date: 'October 20, 2024',
+		date: new Date('October 20, 2024').getTime(),
 		author: 'Michael Brown',
-		category: 'Geothermal Energy',
 	},
 	{
 		id: 'project-4',
 		src: '/projectImage4.jpg',
 		title: 'Hydropower Station Modernization',
-		date: 'September 5, 2024',
+		date: new Date('September 5, 2024').getTime(),
 		author: 'Emily White',
-		category: 'Hydropower',
 	},
 	{
 		id: 'project-5',
 		src: '/projectImage5.jpg',
 		title: 'Community Solar Panel Project',
-		date: 'August 12, 2024',
+		date: new Date('August 12, 2024').getTime(),
 		author: 'Liam Carter',
-		category: 'Solar Energy',
 	},
 	{
 		id: 'project-6',
 		src: '/projectImage6.jpg',
 		title: 'Biomass Energy for Rural Areas',
-		date: 'July 22, 2024',
+		date: new Date('July 22, 2024').getTime(),
 		author: 'Emma Wilson',
-		category: 'Biomass Energy',
 	},
 	{
 		id: 'project-7',
 		src: '/projectImage7.jpg',
 		title: 'Innovative Wave Energy Converter',
-		date: 'June 15, 2024',
+		date: new Date('June 15, 2024').getTime(),
 		author: 'Olivia Martinez',
-		category: 'Wave Energy',
 	},
 	{
 		id: 'project-8',
 		src: '/projectImage8.jpg',
 		title: 'Zero-Energy Building Construction',
-		date: 'May 30, 2024',
+		date: new Date('May 30, 2024').getTime(),
 		author: 'James Lopez',
-		category: 'Sustainable Architecture',
 	},
 	{
 		id: 'project-9',
 		src: '/projectImage9.jpg',
 		title: 'Floating Solar Panel Farms',
-		date: 'April 18, 2024',
+		date: new Date('April 18, 2024').getTime(),
 		author: 'Isabella Thompson',
-		category: 'Solar Energy',
 	},
 	{
 		id: 'project-10',
 		src: '/projectImage10.jpg',
 		title: 'Smart Renewable Energy Grids',
-		date: 'March 10, 2024',
+		date: new Date('March 10, 2024').getTime(),
 		author: 'Noah Johnson',
-		category: 'Smart Grids',
 	},
 ]
 
