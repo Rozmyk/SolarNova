1bac68a4b05617bcb6de6bd61476ebfca9c3db31
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-16 15:17:46 +0100

    Create categoriesData.tsx

---
+++ data/categoriesData.tsx
@@ -0,0 +1,12 @@
+const categoriesData = [
+	{ id: 1, value: 'solar-energy', name: 'Solar Energy' },
+	{ id: 2, value: 'wind-energy', name: 'Wind Energy' },
+	{ id: 3, value: 'geothermal-energy', name: 'Geothermal Energy' },
+	{ id: 4, value: 'hydropower', name: 'Hydropower' },
+	{ id: 5, value: 'biomass-energy', name: 'Biomass Energy' },
+	{ id: 6, value: 'wave-energy', name: 'Wave Energy' },
+	{ id: 7, value: 'sustainable-architecture', name: 'Sustainable Architecture' },
+	{ id: 8, value: 'smart-grids', name: 'Smart Grids' },
+]
+
+export default categoriesData
