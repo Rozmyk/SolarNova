cf774012bf1d9e45907519361d5086073d591f99
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-16 18:07:44 +0100

    change font color

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -6,7 +6,7 @@ const Button = ({ children, reverse, onClick }: { children: string; reverse?: bo
 		<button
 			onClick={onClick}
 			type='button'
-			className='py-2.5 px-6 text-sm bg-[#f7f7f7] text-[#323332] rounded-full cursor-pointer font-regular text-center shadow-xs transition-all duration-500 hover:bg-stone-200 inline-flex items-center gap-1 capitalize'>
+			className='py-2.5 px-6 text-sm bg-[#f7f7f7] text-secondary  rounded-full cursor-pointer font-regular text-center shadow-xs transition-all duration-500 hover:bg-stone-200 inline-flex items-center gap-1 capitalize'>
 			{children}
 			<TbCircleArrowUpRightFilled size={25} color='#40cb7f' />
 		</button>
