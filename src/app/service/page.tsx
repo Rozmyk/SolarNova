0a4bb4894bee728410603b6588a25325ed52c1b1
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-19 15:45:01 +0100

    Add metadata

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -1,5 +1,13 @@
+import ServicePage from '@/components/ServicePage/ServicePage'
+import { Metadata } from 'next'
+export const metadata: Metadata = {
+	title: 'Services',
+	description:
+		'Explore our range of renewable energy services designed to empower a sustainable future. From solar panel installation to energy consulting, we provide tailored solutions for homes, businesses, and communities.',
+}
+
 const page = () => {
-	return <>wrapper</>
+	return <ServicePage />
 }
 
 export default page
