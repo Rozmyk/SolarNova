c9fa7731d7a21593b8b24419354977f469b27695
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-19 18:46:02 +0100

    Add new animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ export default {
 	],
 	theme: {
 		extend: {
+			animation: {
+				'spin-slow': 'spin 20s linear infinite',
+			},
 			colors: {
 				background: 'hsl(var(--background))',
 				foreground: 'hsl(var(--foreground))',
