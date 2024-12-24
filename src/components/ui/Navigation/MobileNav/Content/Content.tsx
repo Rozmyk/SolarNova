57f5aea3f9e5e9abbe2d961a99f170d012a0198c
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-24 14:16:48 +0100

    Add mobile nav logic

--- src/components/ui/Navigation/MobileNav/Content/Content.tsx
+++ src/components/ui/Navigation/MobileNav/Content/Content.tsx
@@ -1,11 +1,29 @@
-const Content = () => {
+import Link from 'next/link'
+import { SetStateAction, Dispatch } from 'react'
+
+const Content = ({ isOpen, setIsOpen }: { isOpen: boolean; setIsOpen: Dispatch<SetStateAction<boolean>> }) => {
+	const links = ['About', 'Projects', 'Service', 'Contact']
+	const handleClose = () => setIsOpen(false)
+
 	return (
 		<div
-			style={{
-				height: 'calc(100vh - 80px)',
-			}}
-			className='fixed top-[80px] h-full  left-0 right-0 bottom-0 bg-background w-screen z-[100] p-3 flex flex-col justify-start items-start '>
-			gaga
+			className={`fixed top-[80px] left-0 h-[calc(100vh-80px)] bg-foreground w-screen z-[100] p-3 flex flex-col justify-start items-start transition-transform duration-300 ${
+				isOpen ? 'translate-x-0' : '-translate-x-full'
+			}`}>
+			<div className='flex flex-col justify-start items-center gap-8 w-full'>
+				<Link onClick={handleClose} className='text-secondary font-semibold text-3xl' href='/'>
+					Home
+				</Link>
+				{links.map(link => (
+					<Link
+						onClick={handleClose}
+						className='text-secondary font-semibold text-3xl'
+						key={link}
+						href={`/${link.toLowerCase()}`}>
+						{link}
+					</Link>
+				))}
+			</div>
 		</div>
 	)
 }
--- src/components/ui/Navigation/MobileNav/MobileNav.tsx
+++ src/components/ui/Navigation/MobileNav/MobileNav.tsx
@@ -1,28 +1,15 @@
 import HomeButton from '../HomeButton/HomeButton'
 import BurgerButton from './BurgerButton/BurgerButton'
 import Content from './Content/Content'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 const MobileNav = () => {
 	const [isOpen, setIsOpen] = useState(false)
-	useEffect(() => {
-		if (isOpen) {
-			document.body.style.position = 'fixed'
-			document.body.style.overflow = 'hidden'
-		} else {
-			document.body.style.position = ''
-			document.body.style.overflow = ''
-		}
 
-		return () => {
-			document.body.style.position = ''
-			document.body.style.overflow = ''
-		}
-	}, [isOpen])
 	return (
 		<div className=' w-full  h-20 flex justify-between items-center px-4 py-2 bg-foreground'>
 			<HomeButton />
 			<BurgerButton isOpen={isOpen} setIsOpen={setIsOpen} />
-			{isOpen && <Content />}
+			<Content isOpen={isOpen} setIsOpen={setIsOpen} />
 		</div>
 	)
 }
