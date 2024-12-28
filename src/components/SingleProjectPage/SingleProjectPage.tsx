a832b9d63e4a03bba01c4319eb9adbebcf5d707c
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-28 11:49:26 +0100

    Add wrapper

--- src/components/SingleProjectPage/SingleProjectPage.tsx
+++ src/components/SingleProjectPage/SingleProjectPage.tsx
@@ -1,6 +1,10 @@
 import Wrapper from '../Wrapper/Wrapper'
 const SingleProjectPage = ({ projectId }: { projectId: string }) => {
-	return <div className='bg-red-400'>{projectId}</div>
+	return (
+		<Wrapper>
+			<div className='bg-red-400'>{projectId}</div>
+		</Wrapper>
+	)
 }
 
 export default SingleProjectPage
