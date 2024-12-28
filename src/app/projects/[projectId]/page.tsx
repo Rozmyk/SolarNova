847e1d5fd582bffd3161910b165925abe08624af
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-28 11:49:34 +0100

    Add metadata

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,5 +1,9 @@
 import SingleProjectPage from '@/components/SingleProjectPage/SingleProjectPage'
 import { Metadata } from 'next'
+export const metadata: Metadata = {
+	title: 'Singe Project',
+	description: 'lorem20',
+}
 const page = ({ params: { projectId } }: { params: { projectId: string } }) => {
 	return <SingleProjectPage projectId={projectId} />
 }
