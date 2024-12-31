8e7b854f1e1d93d2bbc02daef651e704fbb026fe
Michał Rozmysłowski <kymzor@gmail.com>
2024-12-31 13:46:52 +0100

    Create types.tsx

---
+++ src/types.tsx
@@ -0,0 +1,15 @@
+export interface ProjectProps {
+	id: string
+	src: string
+	title: string
+	date: number
+	author: string
+	description: string
+	technologies: string[]
+	features: string[]
+	gallery: string[]
+	location: string
+	powerClass: string
+	commissioning: string
+	status: string
+}
