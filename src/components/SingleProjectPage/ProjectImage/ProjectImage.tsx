6a0610b453e104aa00ce563a8c38fdc23ca3b230
Michał Rozmysłowski <kymzor@gmail.com>
2025-01-02 12:25:08 +0100

    Update ProjectImage.tsx

--- src/components/SingleProjectPage/ProjectImage/ProjectImage.tsx
+++ src/components/SingleProjectPage/ProjectImage/ProjectImage.tsx
@@ -8,11 +8,11 @@ const ProjectImage = ({ title, src, description }: { title: string; src: string;
 		router.push('/contact')
 	}
 	return (
-		<div className=' relative w-full'>
-			<div className='w-3/4 bg-yellow-300 h-96 relative rounded-lg overflow-hidden'>
+		<div className=' relative w-full p-4 mb-72 md:mb-0'>
+			<div className=' w-full md:w-3/4   h-96 relative rounded-lg overflow-hidden'>
 				<Image fill src={src} alt='solar farm' />
 			</div>
-			<div className='w-1/3 backdrop-blur-lg bg-white bg-opacity-90 rounded-md shadow-lg absolute top-10 right-0  p-8    flex justify-center items-start flex-col'>
+			<div className=' w-3/4 md:w-1/3 backdrop-blur-lg bg-white bg-opacity-90 rounded-md shadow-lg absolute  -bottom-1/2	 md:bottom-1/2  md:translate-y-[50%]  right-1/2 md:right-0  p-8    flex justify-center items-start flex-col translate-x-[50%] md:translate-x-0'>
 				<TitleText>{title}</TitleText>
 				<p className='text-[#6f6e6e] text-sm mb-4'>{description}</p>
 				<Button onClick={handleRedirect}>Contact</Button>
