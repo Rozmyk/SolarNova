55f94d7bf6a1a13bba33dea7a463da065ff74254
Michał Rozmysłowski <kymzor@gmail.com>
2025-01-03 13:09:16 +0100

    Update servicesData.tsx

--- data/servicesData.tsx
+++ data/servicesData.tsx
@@ -1,8 +1,8 @@
 const servicesData = [
 	{ text: 'Solar energy', src: '/service/solarEnergy.webp', single: false, href: 'solar-energy' },
 	{ text: 'Wind energy', src: '/service/windEnergy.webp', single: false, href: 'wind-energy' },
-	{ text: 'Hydropower', src: '/service/hydroPower.webp', single: true, href: 'hydropower' },
-	{ text: 'Service', src: '/service/service.jpg', single: true, href: 'free-energy' },
-	{ text: 'Free energy', src: '/backgroundImage1.jpg', single: false, href: 'free-energy' },
+	{ text: 'Module washing', src: '/service/cleanModules.jpg', single: true, href: 'module-washing' },
+	{ text: 'Service', src: '/service/service.jpg', single: true, href: 'service' },
+	{ text: 'Heat pumps', src: '/service/heatPumps.jpg', single: false, href: 'heat-pumps' },
 ]
 export default servicesData
