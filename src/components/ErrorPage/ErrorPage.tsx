44b135613ae0be3b9baf4ca5b876e81beebfafc3
Michał Rozmysłowski <kymzor@gmail.com>
2025-01-03 13:28:21 +0100

    Create ErrorPage.tsx

---
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,26 @@
+import Button from '../ui/Button/Button'
+import { useRouter } from 'next/navigation'
+const ErrorPage = () => {
+	const router = useRouter()
+	const handleRedirect = () => {
+		router.push('/')
+	}
+	return (
+		<section>
+			<div className='py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6'>
+				<div className='mx-auto max-w-screen-sm text-center'>
+					<h1 className='mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary'>404</h1>
+					<p className='mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white'>
+						Something's missing.
+					</p>
+					<p className='mb-4 text-lg font-light text-gray-500 dark:text-gray-400'>
+						Sorry, we can't find that page. You'll find lots to explore on the home page.{' '}
+					</p>
+					<Button onClick={handleRedirect}>Homepage</Button>
+				</div>
+			</div>
+		</section>
+	)
+}
+
+export default ErrorPage
