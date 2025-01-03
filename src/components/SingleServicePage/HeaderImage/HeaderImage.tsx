a7010a950eb72255a8862569900e55f4f82b9591
Michał Rozmysłowski <kymzor@gmail.com>
2025-01-03 18:12:13 +0100

    Change image position

--- src/components/SingleServicePage/HeaderImage/HeaderImage.tsx
+++ src/components/SingleServicePage/HeaderImage/HeaderImage.tsx
@@ -3,7 +3,7 @@ const HeaderImage = ({ src }: { src: string }) => {
 	return (
 		<div>
 			<div className=' w-72 h-72   md:w-96 md:h-96 rounded-full relative mt-10 '>
-				<div className='absolute z-0 top-1/2 left-1/2  rounded-full bg-transparent  border-dashed border-primary opacity-50 border-4 p-4 translate-y-[-50%] translate-x-[-50%] '>
+				<div className='absolute z-0 top-1/2 left-1/2  rounded-full bg-transparent  border-dashed border-primary opacity-50 border-4 p-4 translate-y-[-50%] translate-x-[-50%]  '>
 					<div className='w-72 h-72   md:w-96 md:h-96'></div>
 				</div>
 				<Image className='rounded-full z-10' alt='xd' fill src={src} />
