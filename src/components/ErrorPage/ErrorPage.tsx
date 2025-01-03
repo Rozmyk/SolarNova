c2c1654bf91350fc3e5565d7a20b83000c06f9a0
Michał Rozmysłowski <kymzor@gmail.com>
2025-01-03 14:41:47 +0100

    Change &apos;

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -14,7 +14,7 @@ const ErrorPage = () => {
 						Something's missing.
 					</p>
 					<p className='mb-4 text-lg font-light text-gray-500 dark:text-gray-400'>
-						Sorry, we can't find that page. You'll find lots to explore on the home page.{' '}
+						Sorry, we can&apos;t find that page. You&apos;ll find lots to explore on the home page.{' '}
 					</p>
 					<Button onClick={handleRedirect}>Homepage</Button>
 				</div>
