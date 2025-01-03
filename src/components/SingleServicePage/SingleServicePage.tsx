686301ed94747e6c9b6f15fd03b77df503f80404
Michał Rozmysłowski <kymzor@gmail.com>
2025-01-03 13:37:07 +0100

    Add HelpSection

--- src/components/SingleServicePage/SingleServicePage.tsx
+++ src/components/SingleServicePage/SingleServicePage.tsx
@@ -6,6 +6,7 @@ import { SingleServiceProps } from '@/types'
 import HeaderContent from './HeaderContent/HeaderContent'
 import HeaderImage from './HeaderImage/HeaderImage'
 import ErrorPage from '../ErrorPage/ErrorPage'
+import HelpSection from './HelpSection/HelpSection'
 const SingleServicePage = ({ serviceId }: { serviceId: string }) => {
 	const [singleServiceData, setSingleServiceData] = useState<SingleServiceProps | null>(null)
 	const [noFound, setNoFound] = useState(false)
@@ -26,10 +27,13 @@ const SingleServicePage = ({ serviceId }: { serviceId: string }) => {
 				<ErrorPage />
 			) : (
 				singleServiceData && (
-					<div className='flex justify-between flex-col-reverse md:flex-row items-center gap-20 p-4'>
-						<HeaderContent title={singleServiceData.text} />
-						<HeaderImage src={singleServiceData.src} />
-					</div>
+					<>
+						<div className='flex justify-between flex-col-reverse md:flex-row items-center gap-20 p-4 mb-14'>
+							<HeaderContent title={singleServiceData.text} />
+							<HeaderImage src={singleServiceData.src} />
+						</div>
+						<HelpSection />
+					</>
 				)
 			)}
 		</Wrapper>
