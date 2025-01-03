dbac35d952b095db17b9c21a48f2daafac421859
Michał Rozmysłowski <kymzor@gmail.com>
2025-01-03 18:12:44 +0100

    Delete TitleText

--- src/components/SingleServicePage/HeaderContent/HeaderContent.tsx
+++ src/components/SingleServicePage/HeaderContent/HeaderContent.tsx
@@ -1,9 +1,8 @@
-import TitleText from '@/components/ui/TitleText/TitleText'
 import Button from '@/components/ui/Button/Button'
 const HeaderContent = ({ title }: { title: string }) => {
 	return (
 		<div>
-			<TitleText>{title}</TitleText>
+			<h1 className='text-secondary font-semibold text-5xl mb-4'>{title}</h1>
 			<p className='text-darkText mb-4'>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, sed ullam? Ad unde similique architecto
 				molestias qui aliquid quisquam totam numquam pariatur sint distinctio dolor, labore ipsa dolores deleniti at!
