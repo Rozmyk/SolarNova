faea0ec25c4dd8e52dd34716447aaf45783b2435
Michał Rozmysłowski <kymzor@gmail.com>
2025-01-03 13:36:59 +0100

    Create HelpSection.tsx

---
+++ src/components/SingleServicePage/HelpSection/HelpSection.tsx
@@ -0,0 +1,10 @@
+import TitleText from '@/components/ui/TitleText/TitleText'
+const HelpSection = () => {
+	return (
+		<div className='bg-red-300 p-4'>
+			<TitleText>How can we help you?</TitleText>
+		</div>
+	)
+}
+
+export default HelpSection
