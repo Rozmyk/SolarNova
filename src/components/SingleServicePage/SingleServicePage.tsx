22e89bfaad031974a4e951d863df1b80ddf4a423
Michał Rozmysłowski <kymzor@gmail.com>
2025-01-03 13:08:58 +0100

    Update SingleServicePage.tsx

--- src/components/SingleServicePage/SingleServicePage.tsx
+++ src/components/SingleServicePage/SingleServicePage.tsx
@@ -1,5 +1,38 @@
+'use client'
+import Wrapper from '../Wrapper/Wrapper'
+import servicesData from '../../../data/servicesData'
+import { useState, useEffect } from 'react'
+import { SingleServiceProps } from '@/types'
+import HeaderContent from './HeaderContent/HeaderContent'
+import HeaderImage from './HeaderImage/HeaderImage'
 const SingleServicePage = ({ serviceId }: { serviceId: string }) => {
-	return <div className='bg-yellow-300'>{serviceId}</div>
+	const [singleServiceData, setSingleServiceData] = useState<SingleServiceProps | null>(null)
+	const [noFound, setNoFound] = useState(false)
+	useEffect(() => {
+		const serviceData = findServiceById(serviceId)
+		if (serviceData) {
+			setSingleServiceData(serviceData)
+		} else {
+			setNoFound(true)
+		}
+	}, [serviceId])
+	const findServiceById = (serviceId: string) => {
+		return servicesData.find(project => project.href === serviceId)
+	}
+	return (
+		<Wrapper>
+			{noFound ? (
+				<p>sorry not found</p>
+			) : (
+				singleServiceData && (
+					<div className='flex justify-between items-center gap-20'>
+						<HeaderContent title={singleServiceData.text} />
+						<HeaderImage src={singleServiceData.src} />
+					</div>
+				)
+			)}
+		</Wrapper>
+	)
 }
 
 export default SingleServicePage
