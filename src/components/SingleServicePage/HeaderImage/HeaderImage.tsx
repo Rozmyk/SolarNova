c764ddae86f98d74ccb54c80d22dfe90ac59f074
Michał Rozmysłowski <kymzor@gmail.com>
2025-01-03 14:53:06 +0100

    Change div size

--- src/components/SingleServicePage/HeaderImage/HeaderImage.tsx
+++ src/components/SingleServicePage/HeaderImage/HeaderImage.tsx
@@ -2,8 +2,10 @@ import Image from 'next/image'
 const HeaderImage = ({ src }: { src: string }) => {
 	return (
 		<div>
-			<div className='w-96 h-96 rounded-full relative'>
-				<div className='absolute z-0 top-1/2 left-1/2  rounded-full bg-transparent  border-dashed border-primary opacity-50 border-4 w-[450px] h-[450px] translate-y-[-50%] translate-x-[-50%] '></div>
+			<div className=' w-72 h-72   md:w-96 md:h-96 rounded-full relative mt-10 '>
+				<div className='absolute z-0 top-1/2 left-1/2  rounded-full bg-transparent  border-dashed border-primary opacity-50 border-4 p-4 translate-y-[-50%] translate-x-[-50%] '>
+					<div className='w-72 h-72   md:w-96 md:h-96'></div>
+				</div>
 				<Image className='rounded-full z-10' alt='xd' fill src={src} />
 			</div>
 		</div>
