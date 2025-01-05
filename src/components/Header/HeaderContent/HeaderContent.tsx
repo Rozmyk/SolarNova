53b27621b8d6abc179b1319f5a94b17e100da625
Michał Rozmysłowski <kymzor@gmail.com>
2025-01-05 13:57:42 +0100

    Add animations

--- src/components/Header/HeaderContent/HeaderContent.tsx
+++ src/components/Header/HeaderContent/HeaderContent.tsx
@@ -1,18 +1,41 @@
+'use client'
+import React, { useRef } from 'react'
 import Button from '@/components/ui/Button/Button'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
 
 const HeaderContent = () => {
+	const container = useRef(null)
+	const title = useRef(null)
+	const paragraph = useRef(null)
+	const button = useRef(null)
+
+	useGSAP(() => {
+		gsap.registerPlugin()
+
+		const tl = gsap.timeline()
+		tl.from(title.current, { opacity: 0, y: 50, duration: 0.5 })
+			.from(paragraph.current, { opacity: 0, y: 50, duration: 0.5 }, '-=0.3')
+			.from(button.current, { opacity: 0, y: 50, duration: 0.5 }, '-=0.3')
+	}, [])
+
 	return (
-		<div className=' w-full  md:w-2/3 p-4'>
-			<h1 className=' text-4xl md:text-7xl xl:text-8xl  mb-4 md:mb-8 font-semibold  w-full  text-secondary'>
+		<div ref={container} className='w-full md:w-2/3 p-4'>
+			<h1 ref={title} className='text-4xl md:text-7xl xl:text-8xl mb-4 md:mb-8 font-semibold w-full text-secondary'>
 				More Greener <br />
 				More Renewable <br />
 				Energy
 			</h1>
-			<p className='md:mb-8 mb-4  w-full md:w-1/2  font-regular text-darkText' style={{ fontWeight: 500 }}>
+			<p
+				ref={paragraph}
+				className='md:mb-8 mb-4 w-full md:w-1/2 font-regular text-darkText'
+				style={{ fontWeight: 500 }}>
 				Embrace renewable energy for a sustainable tomorrow. There are many variations of passages of Lorem Ipsum
 				available.
 			</p>
-			<Button>Get a Free Quote</Button>
+			<div ref={button}>
+				<Button>Get a Free Quote</Button>
+			</div>
 		</div>
 	)
 }
--- src/components/Header/HeaderImage/HeaderImage.tsx
+++ src/components/Header/HeaderImage/HeaderImage.tsx
@@ -1,25 +1,47 @@
-import { GoNorthStar } from 'react-icons/go'
+'use client'
+import React, { useRef } from 'react'
 import Image from 'next/image'
+import { GoNorthStar } from 'react-icons/go'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+
 const SingleAvatar = ({ src, alt }: { src: string; alt: string }) => {
 	return <img className='inline-block size-6 rounded-full ring-2 ring-white' src={src} alt={alt} />
 }
+
 const HeaderImage = () => {
+	const imageRef = useRef(null)
+	const avatarsRef = useRef<HTMLDivElement>(null)
+
+	useGSAP(() => {
+		gsap.registerPlugin()
+
+		const tl = gsap.timeline()
+		tl.from(imageRef.current, { scale: 0.8, duration: 1, ease: 'power2.out' })
+
+		if (avatarsRef.current) {
+			tl.from(avatarsRef.current.children, { opacity: 0, y: 50, duration: 0.5, stagger: 0.2 }, '-=0.5')
+		}
+	}, [])
+
 	return (
-		<div className='relative  w-full md:w-1/3 mb-40 md:mb-0 '>
-			<div className='absolute top-2 right-6 animate-spin-fast'>
+		<div className='relative w-full md:w-1/3 mb-40 md:mb-0'>
+			<div className='absolute top-2 right-6 animate-spin-fast z-10'>
 				<GoNorthStar size={100} color='#40cb7f' />
 			</div>
 
-			<Image width={750} height={500} alt='Wind turbine' src='/header/headerImage.webp' />
+			<div ref={imageRef}>
+				<Image width={750} height={500} alt='Wind turbine' src='/header/headerImage.webp' />
+			</div>
 
 			<div className='w-36 h-36 absolute bottom-[-72px] md:bottom-10 left-1/2 translate-x-[-50%] md:left-[-75px] md:translate-x-0 backdrop-blur-lg bg-white bg-opacity-60 flex flex-col justify-center items-center rounded-md shadow-lg'>
-				<div className='flex -space-x-1 overflow-hidden mb-2 p-1'>
+				<div ref={avatarsRef} className='flex -space-x-1 overflow-hidden mb-2 p-1'>
 					<SingleAvatar src='/customers/customerAvatar1.webp' alt='portrait of a woman' />
-					<SingleAvatar src='/customers/customerAvatar2.webp' alt='portrait of a  man' />
+					<SingleAvatar src='/customers/customerAvatar2.webp' alt='portrait of a man' />
 					<SingleAvatar src='/customers/customerAvatar3.webp' alt='portrait of a young man' />
 				</div>
-				<p className=' text-2xl md:text-4xl font-medium text-secondary mb-2'>05K+</p>
-				<p className='text-md  text-[#6f6e6e]' style={{ fontWeight: 500 }}>
+				<p className='text-2xl md:text-4xl font-medium text-secondary mb-2'>05K+</p>
+				<p className='text-md text-[#6f6e6e]' style={{ fontWeight: 500 }}>
 					Customers
 				</p>
 			</div>
