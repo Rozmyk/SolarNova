c04d8b259e45fdb308ac2ab00a0fa71bfbf86e5b
Michał Rozmysłowski <kymzor@gmail.com>
2025-01-06 17:03:49 +0100

    Add Cards animation

--- src/components/Solutions/Cards/Cards.tsx
+++ src/components/Solutions/Cards/Cards.tsx
@@ -1,8 +1,16 @@
+'use client'
+
+import React, { useEffect, useRef } from 'react'
 import { GoArrowUpRight } from 'react-icons/go'
 import { SlEnergy } from 'react-icons/sl'
 import { TbCarFan } from 'react-icons/tb'
 import { RiWaterFlashFill } from 'react-icons/ri'
 import Image from 'next/image'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
 type SingleCardProps = {
 	src: string
 	icon: React.ComponentType<{ size?: number; className?: string }>
@@ -20,7 +28,7 @@ const SingleCard = ({ src, icon: Icon, title, text }: SingleCardProps) => {
 					backgroundSize: 'cover',
 					backgroundPosition: 'center',
 				}}></div>
-			<div className='h-full w-full absolute top-0 bottom-0 left-0 right-0 bg-[#123520] opacity-90 z-10 '></div>
+			<div className='h-full w-full absolute top-0 bottom-0 left-0 right-0 bg-[#123520] opacity-90 z-10'></div>
 
 			<div className='z-20 relative flex flex-col justify-between items-start h-full'>
 				<Icon size={40} className='mb-4' />
@@ -30,23 +38,55 @@ const SingleCard = ({ src, icon: Icon, title, text }: SingleCardProps) => {
 				</div>
 			</div>
 
-			<button className='w-12 h-12 bg-primary rounded-full flex justify-center items-center absolute bottom-0 right-0  z-20'>
+			<button className='w-12 h-12 bg-primary rounded-full flex justify-center items-center absolute bottom-0 right-0 z-20'>
 				<GoArrowUpRight size={20} />
 			</button>
 
-			<Image
-				className='absolute bottom-0 right-0 z-10 '
-				height={80}
-				width={80}
-				alt='decorative'
-				src='/svg/corner.svg'
-			/>
+			<Image className='absolute bottom-0 right-0 z-10' height={80} width={80} alt='decorative' src='/svg/corner.svg' />
 		</div>
 	)
 }
+
 const Cards = () => {
+	const cardsRef = useRef<HTMLDivElement>(null)
+	let observer: IntersectionObserver
+
+	useEffect(() => {
+		if (cardsRef.current) {
+			gsap.set(cardsRef.current.children, { opacity: 0, y: 50 })
+
+			const animate = (target: Element) => {
+				gsap.to(target.children, {
+					opacity: 1,
+					y: 0,
+					duration: 0.5,
+					stagger: 0.2,
+					ease: 'power2.out',
+				})
+			}
+
+			observer = new IntersectionObserver(
+				entries => {
+					entries.forEach(entry => {
+						if (entry.isIntersecting) {
+							animate(entry.target)
+							observer.unobserve(entry.target)
+						}
+					})
+				},
+				{ threshold: 0.5 }
+			)
+
+			observer.observe(cardsRef.current)
+		}
+
+		return () => {
+			if (observer) observer.disconnect()
+		}
+	}, [])
+
 	return (
-		<div className='flex md:flex-row flex-col justify-between items-center md:gap-14 gap-8'>
+		<div ref={cardsRef} className='flex md:flex-row flex-col justify-between items-center md:gap-14 gap-8'>
 			<SingleCard
 				src='/service/solarEnergy.webp'
 				icon={SlEnergy}
