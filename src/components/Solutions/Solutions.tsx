83c04a1779f5e1ab491bd24cdbb6f17f206c937e
Michał Rozmysłowski <kymzor@gmail.com>
2025-01-06 16:29:57 +0100

    add animations

--- src/components/Solutions/Solutions.tsx
+++ src/components/Solutions/Solutions.tsx
@@ -4,11 +4,52 @@ import TitleText from '../ui/TitleText/TitleText'
 import Button from '../ui/Button/Button'
 import Cards from './Cards/Cards'
 import { useRouter } from 'next/navigation'
+import gsap from 'gsap'
+import { useRef, useEffect } from 'react'
+
 const Solutions = () => {
 	const router = useRouter()
+	const solutionsHeader = useRef<HTMLDivElement>(null)
+
+	useEffect(() => {
+		gsap.registerPlugin()
+
+		const element = solutionsHeader.current
+		if (element) {
+			gsap.set(element, { opacity: 0, y: 50 })
+
+			const animate = () => {
+				gsap.to(element, {
+					opacity: 1,
+					y: 0,
+					duration: 0.5,
+					ease: 'power2.out',
+				})
+			}
+
+			const observer = new IntersectionObserver(
+				entries => {
+					entries.forEach(entry => {
+						if (entry.isIntersecting) {
+							animate()
+							observer.unobserve(entry.target)
+						}
+					})
+				},
+				{ threshold: 0.5 }
+			)
+
+			observer.observe(element)
+
+			return () => {
+				observer.disconnect()
+			}
+		}
+	}, [])
+
 	return (
 		<Wrapper>
-			<div className='flex md:flex-row flex-col justify-between items-center gap-4 mb-2 p-4'>
+			<div ref={solutionsHeader} className='flex md:flex-row flex-col justify-between items-center gap-4 mb-2 p-4'>
 				<div className='w-full md:w-1/2'>
 					<TitleText>We Provide Comprehensive Solar Solutions</TitleText>
 				</div>
@@ -21,7 +62,7 @@ const Solutions = () => {
 					</Button>
 				</div>
 			</div>
-			<div className='w-full  p-8'>
+			<div className='w-full p-8'>
 				<Cards />
 			</div>
 		</Wrapper>
