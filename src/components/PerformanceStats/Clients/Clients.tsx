58f077e1adc20b9b8670da707e2a698e0f041dd7
Michał Rozmysłowski <kymzor@gmail.com>
2025-01-06 18:18:56 +0100

    Add GSAP animations

--- src/components/PerformanceStats/Clients/Clients.tsx
+++ src/components/PerformanceStats/Clients/Clients.tsx
@@ -1,11 +1,15 @@
 'use client'
 import Wrapper from '@/components/Wrapper/Wrapper'
 import TitleText from '@/components/ui/TitleText/TitleText'
-import Button from '@/components/ui/Button/Button'
 import Image from 'next/image'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Pagination, Autoplay } from 'swiper/modules'
+import { useRef, useEffect } from 'react'
 import testimonialsData from '../../../../data/testimonialsData'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
 import 'swiper/css'
 import 'swiper/css/pagination'
 import './swiper.css'
@@ -39,17 +43,71 @@ const SingleTestimonials = ({
 	)
 }
 const Clients = () => {
+	const clientsHeader = useRef<HTMLDivElement>(null)
+	const clientsRef = useRef<HTMLDivElement>(null)
+	let observer: IntersectionObserver
+
+	useEffect(() => {
+		if (clientsRef.current) {
+			gsap.set(clientsRef.current.children, { opacity: 0, y: 50 })
+
+			const animateContent = (target: Element) => {
+				gsap.to(target.children, {
+					opacity: 1,
+					y: 0,
+					duration: 0.5,
+					stagger: 0.2,
+					ease: 'power2.out',
+				})
+			}
+
+			observer = new IntersectionObserver(
+				entries => {
+					entries.forEach(entry => {
+						if (entry.isIntersecting) {
+							animateContent(entry.target)
+							observer.unobserve(entry.target)
+						}
+					})
+				},
+				{ threshold: 0.5 }
+			)
+
+			observer.observe(clientsRef.current)
+		}
+
+		if (clientsHeader.current) {
+			gsap.fromTo(
+				clientsHeader.current,
+				{ opacity: 0, y: -50 },
+				{
+					opacity: 1,
+					y: 0,
+					duration: 1,
+					ease: 'power2.out',
+					scrollTrigger: {
+						trigger: clientsHeader.current,
+						start: 'top 80%',
+						end: 'bottom 60%',
+						toggleActions: 'play none none none',
+					},
+				}
+			)
+		}
+
+		return () => {
+			if (observer) observer.disconnect()
+		}
+	}, [])
+
 	return (
 		<Wrapper>
-			<div className='flex md:flex-row flex-col justify-between items-center gap-4 mb-2 p-4'>
+			<div ref={clientsHeader} className='flex md:flex-row flex-col justify-between items-center gap-4 mb-2 p-4'>
 				<div className='w-full md:w-1/2'>
 					<TitleText>Gem Words From Our Satisfied Clients</TitleText>
 				</div>
-				<div className='w-full md:w-1/2 flex justify-end'>
-					<Button>All testimonials</Button>
-				</div>
 			</div>
-			<div className='ExampleComponent flex gap-10  p-2 '>
+			<div ref={clientsRef} className='ExampleComponent flex gap-10  p-2 '>
 				<Swiper
 					breakpoints={{
 						0: {
