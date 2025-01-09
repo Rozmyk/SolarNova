4924a9a5f153c63643e2c9dab096bdc2dfbd9427
Michał Rozmysłowski <kymzor@gmail.com>
2025-01-09 12:40:05 +0100

    fix animation

--- src/components/AboutPage/StatsSection/StatsSection.tsx
+++ src/components/AboutPage/StatsSection/StatsSection.tsx
@@ -20,12 +20,12 @@ const SingleService = ({
 	let observer: IntersectionObserver
 	useEffect(() => {
 		if (tileRef.current) {
-			gsap.set(tileRef.current, { opacity: 0, y: 50 })
+			gsap.set(tileRef.current, { opacity: 0 })
 
 			const animate = (target: Element) => {
 				gsap.to(target, {
 					opacity: 1,
-					y: 0,
+
 					duration: 0.5,
 					ease: 'power2.out',
 				})
