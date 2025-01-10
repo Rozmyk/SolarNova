138e2c6622e697c09da4138bee8154c94ed543c3
Michał Rozmysłowski <kymzor@gmail.com>
2025-01-10 16:30:54 +0100

    Breakdown into components

---
+++ src/components/SingleProjectPage/DetailsSection/DetailsSection.tsx
@@ -0,0 +1,96 @@
+import { useEffect, useRef } from 'react'
+import { ProjectProps } from '@/types'
+import gsap from 'gsap'
+
+const SingleProjectProfile = ({ label, value }: { label: string; value: string }) => {
+	const profileRef = useRef<HTMLDivElement | null>(null)
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			entries => {
+				entries.forEach(entry => {
+					if (entry.isIntersecting) {
+						gsap.fromTo(entry.target, { opacity: 0, y: 50 }, { opacity: 1, y: 0, duration: 0.5, ease: 'power2.out' })
+						observer.unobserve(entry.target)
+					}
+				})
+			},
+			{ threshold: 0.1 }
+		)
+
+		if (profileRef.current) {
+			observer.observe(profileRef.current)
+		}
+
+		return () => {
+			if (profileRef.current) {
+				observer.unobserve(profileRef.current)
+			}
+		}
+	}, [])
+
+	return (
+		<div ref={profileRef} className='text-secondary flex flex-col justify-start items-start text-lg opacity-0'>
+			<p className='font-semibold'>{label}</p>
+			<p>{value}</p>
+			<div className='mt-2 mb-2 border-t border-gray-300 w-full' />
+		</div>
+	)
+}
+
+const DetailsSection = ({ singleProjectData }: { singleProjectData: ProjectProps }) => {
+	const sectionRef = useRef<HTMLDivElement | null>(null)
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			entries => {
+				entries.forEach(entry => {
+					if (entry.isIntersecting) {
+						gsap.fromTo(entry.target, { opacity: 0, y: 50 }, { opacity: 1, y: 0, duration: 1, ease: 'power2.out' })
+						observer.unobserve(entry.target)
+					}
+				})
+			},
+			{ threshold: 0.1 }
+		)
+
+		if (sectionRef.current) {
+			observer.observe(sectionRef.current)
+		}
+
+		return () => {
+			if (sectionRef.current) {
+				observer.unobserve(sectionRef.current)
+			}
+		}
+	}, [])
+
+	return (
+		<div
+			ref={sectionRef}
+			className='w-full mt-20 flex-col md:flex-row md:flex justify-between items-start gap-20 mb-20 p-4 opacity-0'>
+			<div className='w-full md:w-1/2 md:mb-0 mb-10'>
+				<h3 className='text-secondary font-semibold text-xl mb-2'>The mission</h3>
+				<p className='text-darkText'>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi nisi ab quaerat quasi consectetur, eius iste
+					suscipit incidunt perspiciatis a.
+				</p>
+			</div>
+			<div className='w-full md:w-1/2'>
+				<h3 className='text-secondary font-semibold text-xl mb-2'>Project profile</h3>
+				<div className='w-full flex flex-col gap-2'>
+					{singleProjectData && (
+						<>
+							<SingleProjectProfile label='Location:' value={singleProjectData.location} />
+							<SingleProjectProfile label='Power class:' value={singleProjectData.powerClass} />
+							<SingleProjectProfile label='Commissioning: ' value={singleProjectData.commissioning} />
+							<SingleProjectProfile label='Status:' value={singleProjectData.status} />
+						</>
+					)}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default DetailsSection
--- src/components/SingleProjectPage/SingleProjectPage.tsx
+++ src/components/SingleProjectPage/SingleProjectPage.tsx
@@ -3,21 +3,12 @@ import Spinner from '../ui/Spinner/Spinner'
 import Wrapper from '../Wrapper/Wrapper'
 import ProjectImage from './ProjectImage/ProjectImage'
 import projectData from '../../../data/projectsData'
-import { useState, useEffect, } from 'react'
+import { useState, useEffect } from 'react'
 import Technologies from './Technologies/Technologies'
 import Carousel from './Carousel/Carousel'
 import OtherProjects from './OtherProjects/OtherProjects'
 import { ProjectProps } from '@/types'
-
-const SingleProjectProfile = ({ label, value }: { label: string; value: string }) => {
-	return (
-		<div className='text-secondary flex flex-col justify-start items-start text-lg'>
-			<p className='font-semibold'>{label}</p>
-			<p>{value}</p>
-			<div className='mt-2 mb-2 border-t border-gray-300 w-full' />
-		</div>
-	)
-}
+import DetailsSection from './DetailsSection/DetailsSection'
 
 const SingleProjectPage = ({ projectId }: { projectId: string }) => {
 	const [singleProjectData, setSingleProjectData] = useState<ProjectProps | null>(null)
@@ -50,28 +41,7 @@ const SingleProjectPage = ({ projectId }: { projectId: string }) => {
 							src={singleProjectData.src}
 						/>
 					)}
-					<div className='w-full   mt-20 flex-col md:flex-row md:flex justify-between items-start gap-20 mb-20 p-4'>
-						<div className='w-full md:w-1/2 md:mb-0 mb-10 '>
-							<h3 className='text-secondary font-semibold text-xl mb-2'>The mission</h3>
-							<p className='text-darkText '>
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi nisi ab quaerat quasi consectetur, eius
-								iste suscipit incidunt perspiciatis a.
-							</p>
-						</div>
-						<div className=' w-full md:w-1/2  '>
-							<h3 className='text-secondary font-semibold text-xl mb-2'>Project profile</h3>
-							<div className='w-full flex flex-col gap-2 '>
-								{singleProjectData && (
-									<>
-										<SingleProjectProfile label='Location:' value={singleProjectData.location} />
-										<SingleProjectProfile label='Power class:' value={singleProjectData.powerClass} />
-										<SingleProjectProfile label='Commissioning: ' value={singleProjectData.commissioning} />
-										<SingleProjectProfile label='Status:' value={singleProjectData.status} />
-									</>
-								)}
-							</div>
-						</div>
-					</div>
+					{singleProjectData && <DetailsSection singleProjectData={singleProjectData} />}
 					{singleProjectData && <Carousel gallery={singleProjectData.gallery} />}
 					<Technologies />
 
