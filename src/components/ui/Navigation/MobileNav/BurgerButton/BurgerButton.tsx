8679dadd2f67434f4f3bb4f41ffa2bdfded56cd0
Michał Rozmysłowski <kymzor@gmail.com>
2025-01-10 21:36:13 +0100

    Add aria label

--- src/components/ui/Navigation/MobileNav/BurgerButton/BurgerButton.tsx
+++ src/components/ui/Navigation/MobileNav/BurgerButton/BurgerButton.tsx
@@ -5,8 +5,9 @@ const BurgerButton = ({ isOpen, setIsOpen }: { isOpen: boolean; setIsOpen: Dispa
 
 	return (
 		<button
-			className='flex flex-col h-10 w-10   rounded-lg bg-[#ccf2de] justify-center items-center group'
-			onClick={() => setIsOpen(!isOpen)}>
+			className='flex flex-col h-10 w-10 rounded-lg bg-[#ccf2de] justify-center items-center group'
+			onClick={() => setIsOpen(!isOpen)}
+			aria-label={isOpen ? 'Close menu' : 'Open menu'}>
 			<div className='h-10 w-5 flex flex-col items-start justify-center'>
 				<div className={`${genericHamburgerLine} ${isOpen ? 'rotate-45 translate-y-1.5 w-5 ' : 'opacity-100 w-3'}`} />
 				<div className={`${genericHamburgerLine} ${isOpen ? 'opacity-0' : 'opacity-100'}`} />
