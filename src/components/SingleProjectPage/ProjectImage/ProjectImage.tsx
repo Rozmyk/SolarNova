9254272461a81888389bea30d33bc16e5b77d7bc
Michał Rozmysłowski <kymzor@gmail.com>
2025-01-10 16:35:42 +0100

    fix Image

--- src/components/SingleProjectPage/ProjectImage/ProjectImage.tsx
+++ src/components/SingleProjectPage/ProjectImage/ProjectImage.tsx
@@ -1,19 +1,22 @@
-import Image from 'next/image'
-import Button from '@/components/ui/Button/Button'
-import TitleText from '@/components/ui/TitleText/TitleText'
 import { useRouter } from 'next/navigation'
 import { useRef } from 'react'
+import Image from 'next/image'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
-const ProjectImage = ({ title, src, description }: { title: string; src: string; description: string }) => {
+import TitleText from '@/components/ui/TitleText/TitleText'
+import Button from '@/components/ui/Button/Button'
+
+const ProjectImage = ({ title, description, src }: { title: string; description: string; src: string }) => {
 	const router = useRouter()
 	const imageRef = useRef(null)
 	const titleRef = useRef(null)
 	const descriptionRef = useRef(null)
 	const buttonRef = useRef(null)
+
 	const handleRedirect = () => {
 		router.push('/contact')
 	}
+
 	useGSAP(() => {
 		gsap.registerPlugin()
 
@@ -25,12 +28,13 @@ const ProjectImage = ({ title, src, description }: { title: string; src: string;
 			.from(descriptionRef.current, { opacity: 0, y: 50, duration: 0.5 }, '-=0.3')
 			.from(buttonRef.current, { opacity: 0, y: 50, duration: 0.5 }, '-=0.3')
 	}, [])
+
 	return (
-		<div className=' relative w-full p-4 mb-72 md:mb-0'>
-			<div ref={imageRef} className=' w-full md:w-3/4   h-96 relative rounded-lg overflow-hidden'>
-				<Image fill src={src} alt='solar farm' />
+		<div className='relative w-full p-4 mb-72 md:mb-0'>
+			<div ref={imageRef} className='w-full md:w-3/4 h-96 relative rounded-lg overflow-hidden'>
+				<Image fill src={src} alt='solar farm' style={{ objectFit: 'cover', objectPosition: 'center' }} />
 			</div>
-			<div className=' w-3/4 md:w-1/3 backdrop-blur-lg bg-white bg-opacity-90 rounded-md shadow-lg absolute  -bottom-1/2	 md:bottom-1/2  md:translate-y-[50%]  right-1/2 md:right-0  p-8    flex justify-center items-start flex-col translate-x-[50%] md:translate-x-0'>
+			<div className='w-3/4 md:w-1/3 backdrop-blur-lg bg-white bg-opacity-90 rounded-md shadow-lg absolute -bottom-1/2 md:bottom-1/2 md:translate-y-[50%] right-1/2 md:right-0 p-8 flex justify-center items-start flex-col translate-x-[50%] md:translate-x-0'>
 				<div ref={titleRef}>
 					<TitleText>{title}</TitleText>
 				</div>
