6a0a38d0f14d609c6fcc0f454f7ac376bcbe494f
Michał Rozmysłowski <kymzor@gmail.com>
2025-01-10 20:56:54 +0100

    Add aria label

--- src/components/ui/CarouselButtons/CarouselButtons.tsx
+++ src/components/ui/CarouselButtons/CarouselButtons.tsx
@@ -6,10 +6,16 @@ const CarouselButtons = ({ onPrev, onNext }: { onPrev: () => void; onNext: () =>
 		<div className='flex justify-center items-center gap-4'>
 			<button
 				onClick={onPrev}
-				className=' w-10 h-10 rounded-full bg-transparent  border-primary border border-solid flex justify-center items-center'>
+				className='w-10 h-10 rounded-full bg-transparent border-primary border border-solid flex justify-center items-center'
+				aria-label='Previous slide'
+				role='button'>
 				<MdKeyboardArrowLeft size={20} color='#40cb7f' />
 			</button>
-			<button onClick={onNext} className='w-10 h-10 rounded-full bg-primary flex justify-center items-center'>
+			<button
+				onClick={onNext}
+				className='w-10 h-10 rounded-full bg-primary flex justify-center items-center'
+				aria-label='Next slide'
+				role='button'>
 				<MdKeyboardArrowRight size={20} />
 			</button>
 		</div>
